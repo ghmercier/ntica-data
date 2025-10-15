cbcdb1c0b7185ccc7ab480c7a732f85ebbd0c503
Ghislain Mercier <ghislain.mercier@club-internet.fr>
2025-10-15 20:10:28 +0000

    Change shell (sh > bash) in init-trainees.sh

--- sspcloud/init-trainees.sh
+++ sspcloud/init-trainees.sh
@@ -7,7 +7,7 @@ export MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 sh $MY_REPO/sspcloud/restore_environment.sh
 
 # Download data
-sh $MY_REPO/sspcloud/download_data.sh
+bash $MY_REPO/sspcloud/download_data.sh
 
 # Download the notebook
 # $1 is an argument giving the path of the notebook within the website (eg, notebooks/notebook1.ipynb)
